bd8b24243d8a2f87214dbfa46b8618e10bdff5f1
Hai Long Do <long2qw@gmail.com>
2023-09-21 12:58:29 -0400

    chore:create components for the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About'
+import Nav from './components/Nav'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Home />
+    )
+  },
+  {
+    path: "/about",
+    element: (
+      <About />
+    )
+  }
+
+])
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Nav />
+      <RouterProvider router={router} />
     </div>
   );
 }
